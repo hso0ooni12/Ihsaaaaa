85b4f6283272c24d37605ad8d01d49d310cc9092
hso0ooni12 <102685479+hso0ooni12@users.noreply.github.com>
2026-08-17 14:19:03 +0300

    Add files via upload

--- MasahAlOsrah/ContentView.swift
+++ MasahAlOsrah/ContentView.swift
@@ -5,8 +5,10 @@ struct ContentView: UIViewRepresentable {
     func makeUIView(context: Context) -> WKWebView {
         let web = WKWebView()
         web.allowsBackForwardNavigationGestures = true
-        web.load(URLRequest(url: URL(string:"https://fesurvey.stats.gov.sa/family/auth/user")!))
+        web.load(URLRequest(url: URL(string:
+        "https://fesurvey.stats.gov.sa/family/auth/user")!))
         return web
     }
+
     func updateUIView(_ uiView: WKWebView, context: Context) {}
 }
